9be832feefcd17bdb6694882d5b3252429d97254
Kusachev Sergey <s7095509@yandex.ru>
2018-11-23 22:42:51 +0300

    add nested JSON test

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,8 +5,7 @@ const before = '__tests__/__fixtures__/before';
 const after = '__tests__/__fixtures__/after';
 const res = '__tests__/__fixtures__/result';
 
-
-describe('diff', () => {
+describe('diff flat', () => {
   it('simple JSON -> JSON', () => {
     const expected = readFileSync(res, 'utf8');
     expect(gendiff(`${before}.json`, `${after}.json`)).toBe(expected);
@@ -20,5 +19,12 @@ describe('diff', () => {
   it('simple ini -> ini', () => {
     const expected = readFileSync(res, 'utf8');
     expect(gendiff(`${before}.ini`, `${after}.ini`)).toBe(expected);
+  });
+});
+
+describe('diff nested', () => {
+  it('nested JSON -> JSON', () => {
+    const expected = readFileSync(`${res}Nested`, 'utf8');
+    expected(gendiff(`${before}Nested.json`, `${after}Nested.json`)).toBe(expected);
   });
 });
