c26e8028b07d1d32482ed6b72bcbe8b9de266e2c
Kusachev Sergey <s7095509@yandex.ru>
2018-11-23 19:25:29 +0300

    fix test

--- __tests__/test.js
+++ __tests__/test.js
@@ -6,16 +6,12 @@ const after = '__tests__/__fixtures__/after';
 const res = '__tests__/__fixtures__/result';
 const expected = fs.readFileSync(res, 'utf8');
 
-describe('show diff', () => {
-  it('simple json -> json', () => {
+describe('simple diff', () => {
+  it('simple', () => {
     const json = gendiff(`${before}.json`, `${after}.json`);
     expect(json).toBe(expected);
-  });
-  it('simple yml -> yml', () => {
     const yml = gendiff(`${before}.yml`, `${after}.yml`);
     expect(yml).toBe(expected);
-  });
-  it('simple ini -> ini', () => {
     const ini = gendiff(`${before}.ini`, `${after}.ini`);
     expect(ini).toBe(expected);
   });
