ffadf3783b795b36d6bccaabf9215cdbe557f70d
Kusachev Sergey <s7095509@yandex.ru>
2018-11-23 19:22:31 +0300

    fix test

--- __tests__/test.js
+++ __tests__/test.js
@@ -4,21 +4,19 @@ import gendiff from '../src';
 const before = '__tests__/__fixtures__/before';
 const after = '__tests__/__fixtures__/after';
 const res = '__tests__/__fixtures__/result';
+const expected = fs.readFileSync(res, 'utf8');
 
 describe('show diff', () => {
   it('simple json -> json', () => {
     const json = gendiff(`${before}.json`, `${after}.json`);
-    const expected = fs.readFileSync(res, 'utf8');
     expect(json).toBe(expected);
   });
   it('simple yml -> yml', () => {
     const yml = gendiff(`${before}.yml`, `${after}.yml`);
-    const expected = fs.readFileSync(res, 'utf8');
     expect(yml).toBe(expected);
   });
   it('simple ini -> ini', () => {
     const ini = gendiff(`${before}.ini`, `${after}.ini`);
-    const expected = fs.readFileSync(res, 'utf8');
     expect(ini).toBe(expected);
   });
 });
