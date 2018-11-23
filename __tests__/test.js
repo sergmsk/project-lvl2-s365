8cc8a0911fb706be60489b32fbd4f43836a26284
Kusachev Sergey <s7095509@yandex.ru>
2018-11-23 21:14:31 +0300

    fix test

--- __tests__/test.js
+++ __tests__/test.js
@@ -4,10 +4,11 @@ import gendiff from '../src';
 const before = '__tests__/__fixtures__/before';
 const after = '__tests__/__fixtures__/after';
 const res = '__tests__/__fixtures__/result';
-const expected = readFileSync(res, 'utf8');
+
 
 describe('simple diff', () => {
   it('simple', () => {
+    const expected = readFileSync(res, 'utf8');
     const json = gendiff(`${before}.json`, `${after}.json`);
     expect(json).toBe(expected);
     const yml = gendiff(`${before}.yml`, `${after}.yml`);
