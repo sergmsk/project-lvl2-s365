1a75465e9d99fdb946f13b99f978eb6b80d3076b
sergmsk <s7095509@yandex.ru>
2018-11-25 19:46:42 +0300

    fix variable names

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { readFileSync } from 'fs';
 import { extname } from 'path';
-import parsers from './parsers';
+import getJSObj from './parsers';
 import makeAST from './ast';
 import render from './render'
 
 export default (firstFile, secondFile, format) => {
-  const first = parsers(extname(firstFile), readFileSync(firstFile, 'utf-8'));
-  const second = parsers(extname(secondFile), readFileSync(secondFile, 'utf-8'));
-  const ast = makeAST(first, second);
+  const firstObj = getJSObj(extname(firstFile), readFileSync(firstFile, 'utf-8'));
+  const secondObj = getJSObj(extname(secondFile), readFileSync(secondFile, 'utf-8'));
+  const ast = makeAST(firstObj, secondObj);
   const res = render(ast, format = 'tree');
   return res;
 };
