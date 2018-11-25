e5af8a6228dee8e575c97e1efb6d6ff9701d665a
Kusachev Sergey <s7095509@yandex.ru>
2018-11-25 07:48:07 +0300

    Ast (#1)
    * add nested test
    
    * add makeAST
    
    * fix ast

--- __tests__/test.js
+++ __tests__/test.js
@@ -11,20 +11,20 @@ describe('diff flat', () => {
     expect(gendiff(`${before}.json`, `${after}.json`)).toBe(expected);
   });
 
-  it('simple yml -> yml', () => {
-    const expected = readFileSync(res, 'utf8');
-    expect(gendiff(`${before}.yml`, `${after}.yml`)).toBe(expected);
-  });
+  // it('simple yml -> yml', () => {
+  //   const expected = readFileSync(res, 'utf8');
+  //   expect(gendiff(`${before}.yml`, `${after}.yml`)).toBe(expected);
+  // });
 
-  it('simple ini -> ini', () => {
-    const expected = readFileSync(res, 'utf8');
-    expect(gendiff(`${before}.ini`, `${after}.ini`)).toBe(expected);
-  });
+  // it('simple ini -> ini', () => {
+  //   const expected = readFileSync(res, 'utf8');
+  //   expect(gendiff(`${before}.ini`, `${after}.ini`)).toBe(expected);
+  // });
 });
 
-// describe('diff nested', () => {
-//   it('nested JSON -> JSON', () => {
-//     const expected = readFileSync(`${res}Nested`, 'utf8');
-//     expected(gendiff(`${before}Nested.json`, `${after}Nested.json`)).toBe(expected);
-//   });
-// });
+describe('diff nested', () => {
+  it('nested JSON -> JSON', () => {
+    const expected = readFileSync(`${res}Nested`, 'utf8');
+    expected(gendiff(`${before}Nested.json`, `${after}Nested.json`)).toBe(expected);
+  });
+});
