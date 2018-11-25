795d8dbff9930e77b6a108eb4ae5d350e09991a0
sergmsk <s7095509@yandex.ru>
2018-11-25 20:31:49 +0300

    fix fix lint problem

--- src/ast.js
+++ src/ast.js
@@ -3,36 +3,36 @@ import _ from 'lodash';
 const dispatcher = [
   {
     type: 'nested',
-    check: (f, s, key) => _.isObject(f[key]) && _.isObject(s[key]),
-    getState: (f, s, funcAST) => ({ children: funcAST(f, s) }),
+    check: (obj1, obj2, key) => _.isObject(obj1[key]) && _.isObject(obj2[key]),
+    getState: (obj1, obj2, obj1uncAST) => ({ children: obj1uncAST(obj1, obj2) }),
   },
   {
     type: 'added',
-    check: (f, s, key) => !_.has(f, key) && _.has(s, key),
-    getState: (f, s) => ({ value: s }),
+    check: (obj1, obj2, key) => !_.has(obj1, key) && _.has(obj2, key),
+    getState: (obj1, obj2) => ({ value: obj2 }),
   },
   {
     type: 'removed',
-    check: (f, s, key) => _.has(f, key) && !_.has(s, key),
-    getState: (f, s) => ({ value: f }),
+    check: (obj1, obj2, key) => _.has(obj1, key) && !_.has(obj2, key),
+    getState: obj1 => ({ value: obj1 }),
   },
   {
     type: 'changed',
-    check: (f, s, key) => _.has(f, key) && _.has(s, key) && (f[key] !== s[key]),
-    getState: (f, s) => ({ before: f, after: s }),
+    check: (obj1, obj2, key) => _.has(obj1, key) && _.has(obj2, key) && (obj1[key] !== obj2[key]),
+    getState: (obj1, obj2) => ({ beobj1ore: obj1, aobj1ter: obj2 }),
   },
   {
     type: 'unchanged',
-    check: (f, s, key) => _.has(f, key) && _.has(s, key) && (f[key] === s[key]),
-    getState: (f, s) => ({ value: f }),
+    check: (obj1, obj2, key) => _.has(obj1, key) && _.has(obj2, key) && (obj1[key] === obj2[key]),
+    getState: obj1 => ({ value: obj1 }),
   },
 ];
 
-const makeAST = (before, after) => {
-  const keys = _.union(Object.keys(before), Object.keys(after));
+const makeAST = (obj1, obj2) => {
+  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
   return keys.map((key) => {
-    const { type, getState } = dispatcher.find(disp => disp.check(before, after, key));
-    const state = getState(before[key], after[key], makeAST);
+    const { type, getState } = dispatcher.find(disp => disp.check(obj1, obj2, key));
+    const state = getState(obj1[key], obj2[key], makeAST);
     return ({ key, type, ...state });
   });
 };
