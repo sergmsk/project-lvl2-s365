402fa34ab3a35dac684dc6b3e5bcab9c9e715e87
sergmsk <s7095509@yandex.ru>
2018-11-25 20:24:07 +0300

    fix lint

--- src/render/index.js
+++ src/render/index.js
@@ -1,14 +1,11 @@
 import renderAsTree from './tree';
 
-const choiceRenderFormat = format => {
-  const handler = {
-    'tree' : renderAsTree,
-  }
+const choiceRenderFormat = (format) => {
+  const handler = { tree: renderAsTree };
   return handler[format];
-}
+};
 
 export default (ast, format = 'tree') => {
   const rend = choiceRenderFormat(format);
   return rend(ast);
 };
-
