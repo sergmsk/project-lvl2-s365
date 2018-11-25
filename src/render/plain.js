305f1e3f23fc0ac5a4a89a5b9780f0a64ca1c95b
sergmsk <s7095509@yandex.ru>
2018-11-25 23:49:01 +0300

    review renderAsPlain

--- src/render/plain.js
+++ src/render/plain.js
@@ -2,35 +2,25 @@ import _ from 'lodash';
 
 const getValue = (arg) => {
   const types = {
-    boolean: (`'${arg}'`),
-    string: (`'${arg}'`),
-    object: 'complex value',
+    boolean: arg,
+    number: arg,
+    string: `'${arg}'`,
+    object: '[complex value]',
   };
   return types[typeof arg];
 };
 
-const renderAsPlain = (ast, parent) => {
-  const getString = (el, body = '') => `Property '${parent}${el.key}' was ${el.type}${body}`;
-  const toString = (element) => {
-    const handler = {
-      nested: el => `${renderAsPlain(el.children, `${parent}${el.key}.`)}`,
-      removed: el => getString(el),
-      modified: (el) => {
-        const body = `. From ${getValue(el.before)} to ${getValue(el.after)}`;
-        return getString(el, body);
-      },
-      added: (el) => {
-        const stringify = ` with ${_.isObject(el.value)
-          ? '[complex value]'
-          : `value: ${getValue(el.value)}`}`;
-        return getString(el, stringify);
-      },
-    };
-    return handler[element.type](element);
+const renderAsPlain = (ast, parent = '') => {
+  const getFromTo = el => `From ${getValue(el.before)} to ${getValue(el.after)}`;
+  const getStringAddedWith = el => (_.isObject(el.value) ? '[complex value]' : `value: ${getValue(el.value)}`);
+  const handlerAction = {
+    nested: el => `${renderAsPlain(el.children, `${parent}${el.key}.`)}`,
+    changed: el => `Property '${parent}${el.key}' was update. ${getFromTo(el)}`,
+    added: el => `Property '${parent}${el.key}' was added with ${getStringAddedWith(el)}`,
+    removed: el => `Property '${parent}${el.key}' was removed`,
   };
-
-  const filtered = ast.filter(element => element.type !== 'unchanged');
-  return filtered.map(element => toString(element)).join('\n');
+  const filtered = ast.filter(el => el.type !== 'unchanged');
+  return filtered.map(el => handlerAction[el.type](el)).join('\n');
 };
 
 export default renderAsPlain;
