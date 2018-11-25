4d58860d176aeaa3cf9feab18a0b3033e2242bd4
sergmsk <s7095509@yandex.ru>
2018-11-25 22:09:44 +0300

    fix plain

--- src/render/plain.js
+++ src/render/plain.js
@@ -11,23 +11,26 @@ const getValue = (arg) => {
 
 const renderAsPlain = (ast, parent) => {
   const getString = (el, body = '') => `Property '${parent}${el.key}' was ${el.type}${body}`;
-  const cases = {
-    nested: el => `${renderAsPlain(el.children, `${parent}${el.key}.`)}`,
-    modified: (el) => {
-      const body = `. From ${getValue(el.before)} to ${getValue(el.after)}`;
-      return getString(el, body);
-    },
-    added: (el) => {
-      const stringify = ` with ${_.isObject(el.value)
-        ? 'complex value'
-        : `value: ${getValue(el.value)}`}`;
-      return getString(el, stringify);
-    },
-    removed: el => getString(el),
+  const toString = (element) => {
+    const handler = {
+      nested: el => `${renderAsPlain(el.children, `${parent}${el.key}.`)}`,
+      removed: el => getString(el),
+      modified: (el) => {
+        const body = `. From ${getValue(el.before)} to ${getValue(el.after)}`;
+        return getString(el, body);
+      },
+      added: (el) => {
+        const stringify = ` with ${_.isObject(el.value)
+          ? '[complex value]'
+          : `value: ${getValue(el.value)}`}`;
+        return getString(el, stringify);
+      },
+    };
+    return handler[element.type](element);
   };
 
   const filtered = ast.filter(element => element.type !== 'unchanged');
-  return filtered.map(node => cases[node.nodeType](node)).join('\n');
+  return filtered.map(element => toString(element)).join('\n');
 };
 
 export default renderAsPlain;
