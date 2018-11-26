65a46f5b43197e3f2742501ae3c57f5e6b711d81
Kusachev Sergey <s7095509@yandex.ru>
2018-11-26 14:34:12 +0300

    Update gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import gendiff from '..';
 
 program
-  .version('0.0.12')
+  .version('0.0.17')
   .description('Compares two configuration files and shows a difference.')
   .option('-f --format [type]', 'output format: tree, plain, json', 'tree')
   .arguments('<firstConfig> <secondConfig>')
